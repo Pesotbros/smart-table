998ca836debb262f7204ae39e7788f801fe5127f
Pesotbros <53221802+Pesotbros@users.noreply.github.com>
2025-11-03 23:57:03 +0500

    Add files via upload

--- src/components/searching.js
+++ src/components/searching.js
@@ -1,11 +1,15 @@
 import {rules, createComparison} from "../lib/compare.js";
 
-
 export function initSearching(searchField) {
     // @todo: #5.1 — настроить компаратор
+    const compare = createComparison([
+        rules.skipEmptyTargetValues,                              // пропускаем пустые значения
+        rules.searchMultipleFields(searchField, ['date', 'customer', 'seller'], false)  
+        // ищем в нескольких полях (дата, покупатель, продавец)
+    ]);
 
     return (data, state, action) => {
         // @todo: #5.2 — применить компаратор
-        return data;
+        return data.filter(row => compare(row, state));
     }
 }
