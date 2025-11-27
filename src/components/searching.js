38e387452b1addcc5716f51ca7c25699564b5fbb
Pesotbros <53221802+Pesotbros@users.noreply.github.com>
2025-11-27 06:17:02 +0500

    Update searching.js

--- src/components/searching.js
+++ src/components/searching.js
@@ -1,15 +1,9 @@
-import {rules, createComparison} from "../lib/compare.js";
-
 export function initSearching(searchField) {
-    // @todo: #5.1 — настроить компаратор
-    const compare = createComparison([
-        rules.skipEmptyTargetValues,                              // пропускаем пустые значения
-        rules.searchMultipleFields(searchField, ['date', 'customer', 'seller'], false)  
-        // ищем в нескольких полях (дата, покупатель, продавец)
-    ]);
-
-    return (data, state, action) => {
-        // @todo: #5.2 — применить компаратор
-        return data.filter(row => compare(row, state));
-    }
+    return (query, state, action) => {
+        return state[searchField] 
+            ? Object.assign({}, query, {
+                search: state[searchField]
+            }) 
+            : query;
+    };
 }
